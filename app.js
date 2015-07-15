3f9b05bead4d54249122f8f3a582cbc7cc3ba38c
survivechris <survival_of_chris@Hotmail.com>
2015-07-15 15:32:34 +0800

    Update app.js
    all attribute are added

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module("urpoApp", ["xeditable", "ui.bootstrap"]);
+var app = angular.module("urpoApp", ["xeditable"]);
 
 app.run(function(editableOptions) {
   editableOptions.theme = 'default'; // bootstrap3 theme. Can be also 'bs2', 'default'
@@ -11,7 +11,54 @@ app.controller('Ctrl', function($scope) {
     Project_Name: '我是專案名稱',
     Project_Description:'我是描述',
     Institution:"CMU中國醫藥大學",
-    Estimated_Start_Date: new Date(1984, 4, 15)
+    Estimated_Start_Date: new Date(1984, 4, 15),
+    Estimated_End_Date: new Date(1984, 4, 15),
+    Region:"Taiwan",
+    Competence:" competence?",
+    Collaboration_Model:"",
+    Project_Agreement_Status:"",
+    Currency:"",
+    Cash_Funding:"",
+    Project_Owners:[{"Name":"林岳暉","EmpID":"622828"}],
+    Principal_Investigators:[
+    	{
+		"Name":"邱俊誠",
+		"Email":"T17988@mail.cmuh.org.tw",
+		"Website":""
+		}
+    ],
+    Milestone:[
+    {
+    	"name":"Planning",
+    	"start_time":"Planning",
+    	"end_time":"Planning",
+    	"status":"",
+    	"description":"",
+    	"file":[
+    		{
+    			"file_name":"tmp.txt",
+    			"file_path":"/dir1/dir2/tmp.txt"
+    		},
+    		{
+    			"file_name":"tmp2.txt",
+				"file_path":"/dir1/dir2/tmp2.txt"
+    		}
+    	]
+    }],
+    edit_state:[
+    	{
+    		"add_or_edit":"edit",
+			"edit_person":"林岳暉",
+			"edit_column":"Project_Description"
+    	},
+    	{
+    		"add_or_edit":"add",
+			"edit_person":"Joan",
+			"edit_column":"Project_Name"
+    	}
+    ],
+    file_system:{},
+    memo:"Hello world!!!"
   }; 
   
 });
